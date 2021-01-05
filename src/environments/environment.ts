c4fd7a03bd52ebad9c13b366ddc59707819f9a3a
Rohit <Rohit_Bhargava_Freelane@outlook.com>
2021-01-05 17:12:49 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDReJtd-kQLjv2EEidxZR8EgS67uO6vZLQ",
+    authDomain: "fir-firestor-ae458.firebaseapp.com",
+    projectId: "fir-firestor-ae458",
+    storageBucket: "fir-firestor-ae458.appspot.com",
+    messagingSenderId: "950541425990",
+    appId: "1:950541425990:web:13d87995bcf749e3dc1947",
+    measurementId: "G-8J5YSKC52K"
+    
+  }
+};
